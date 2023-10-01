c87ba88dd79bf41e094b17f19a970a9de04504d0
Tui <catintheboxx@Tuis-MacBook-Pro.local>
2023-10-01 22:58:50 +0700

    add slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ feature_values = {}
 # Iterate through features
 for feature in features:
     # Add a slider for each feature
-    feature_values[feature] = st.sidebar.slider(f'{feature}', min_value=0.0, max_value=50000.0, value=50.0, step=0.0000000001)
+    feature_values[feature] = st.sidebar.slider(f'{feature}', min_value=0.0, max_value=50000.0, value=50.0, step=0.00000000000000000000001)
 
 # Assuming you have a function to make predictions
 def predict(feature_values):
@@ -28,5 +28,5 @@ def predict(feature_values):
 prediction = predict(feature_values)
 
 # Display the prediction
-st.title('Water Quality Predictor')
+st.title('Water hardness, a measure of mineral content')
 st.write(f"The predicted value is: {prediction}")
