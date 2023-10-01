5f568ff1e976f5b426ffd93eb384ef1ea7476394
Tui <catintheboxx@Tuis-MacBook-Pro.local>
2023-10-01 23:02:24 +0700

    add slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,25 @@ import joblib
 # Load the regression model
 model = joblib.load('regression_model.pkl')  # Replace with your model file path
 
-# Define the features
+# Feature names
 features = ['ph', 'Solids', 'Chloramines', 'Sulfate', 'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity']
 
-# Create a sidebar with slider bars for each feature
-st.sidebar.header('Feature Values')
+# Title and instructions
+st.title("Water Quality Prediction App")
+st.write("Enter the values for the following features:")
 
-# Initialize a dictionary to store feature values
-feature_values = {}
-
-# Iterate through features
+# User input for features
+user_input = {}
 for feature in features:
-    # Add a slider for each feature
-    feature_values[feature] = st.sidebar.slider(f'{feature}', min_value=0.0, max_value=50000.0, value=50.0, step=0.00000000001)
+    user_input[feature] = st.number_input(feature, min_value=0.0)
 
-# Assuming you have a function to make predictions
-def predict(feature_values):
-    new_data = [[feature_values[feature] for feature in features]]
+# When the user clicks the "Predict" button
+if st.button("Predict"):
+    # Prepare the data for prediction
+    new_data = [[user_input[feature] for feature in features]]
+    
+    # Make a prediction
     prediction = model.predict(new_data)
-    return prediction[0]
-
-# Make prediction
-prediction = predict(feature_values)
-
-# Display the prediction
-st.title('Water hardness, a measure of mineral content')
-st.write(f"The predicted value is: {prediction}")
+    
+    # Display the prediction
+    st.write(f"The predicted value is: {prediction[0]}")
