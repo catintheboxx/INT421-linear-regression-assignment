a2fd284c111381645ac8e36fe93314ef2e3c0d83
Tui <catintheboxx@Tuis-MacBook-Pro.local>
2023-10-01 22:49:27 +0700

    add slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,20 @@ import joblib
 # Load the regression model
 model = joblib.load('regression_model.pkl')  # Replace with your model file path
 
+# Define the features
+features = ['ph', 'Solids', 'Chloramines', 'Sulfate', 'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity']
+
+# Set up Streamlit app
 st.title('Water Quality Predictor')
 
-# Define the features
-features = ['ph', 'Solids', 'Chloramines', 'Sulfate', 'Conductivity',
-            'Organic_carbon', 'Trihalomethanes', 'Turbidity']
-
 # Create sliders for each feature
-sliders = {feature: st.slider(f'Select {feature}', min_value=0.0, max_value=100000.0, step=0.1, value=0.0)
-           for feature in features}
+feature_values = []
+for feature in features:
+    value = st.slider(f'Select value for {feature}', min_value=0.0, max_value=100000.0, step=0.01, key=feature)
+    feature_values.append(value)
 
-# Convert slider values to a list for prediction
-new_data = [[sliders[feature] for feature in features]]
-
-# Make a prediction
-prediction = model.predict(new_data)
-
-# Display the prediction
-st.write(f"The predicted value is: {prediction[0]}")
+# Create a button to trigger prediction
+if st.button('Predict'):
+    new_data = [feature_values]
+    prediction = model.predict(new_data)
+    st.write(f"The predicted value is: {prediction[0]}")
